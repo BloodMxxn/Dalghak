a6fef3980824cabdf2d92886c4b7cedfbea94a0a
bloodm00n <bloodm0x0n@gmail.com>
2025-07-30 09:27:03 +0330

    first commit

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/dalghak",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My PWA",
+        short_name: "My PWA",
+        theme_color: "#ffffff",
+        display: "fullscreen",
+        orientation: "portrait",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
